ccf9398101eb0eb584d232443269a485d605cdcb
Chris <christianjuanrios24@gmail.com>
2026-04-15 12:24:28 -0300

    Subo ejercicios de Node

---
+++ ejercicio4.js
@@ -0,0 +1,6 @@
+const calc = require('./calculos');
+
+console.log("Suma:", calc.suma(5, 3));
+console.log("Resta:", calc.resta(8, 6));
+console.log("Multiplicación:", calc.multiplicacion(3, 11));
+console.log("División:", calc.division(30, 5));
